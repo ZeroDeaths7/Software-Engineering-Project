6535e7dbc95c248badf5e057929a2628a9ea5fe9
MJenius <mjenius1357@gmail.com>
2025-11-05 19:18:23 +0530

    New UI And Pics Can Be Added

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.static('public'));
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
